ef3548982ac54df09110ffa2e4f4bbd23d6d14a6
pcbennett108 <pcbennett42@gmail.com>
2023-07-25 15:35:46 -0600

    Add tests and setup for merchant dashboard

---
+++ spec/features/merchants/dashboard_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "merchant dashboard", type: :feature do
+  before(:each) do
+    @merchant = Merchant.create!(name: "Homer's Glassware")
+    
+  end
+
+#   As a merchant,
+# When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+# Then I see the name of my merchant
+
+  describe "as a merchant" do
+    it "when I visit my merchant ddashboard I see my merchant name" do
+      visit "/merchants/#{@merchant.id}/dashboard"
+
+      expect(page).to have_content(@merchant.name)
+    end
+  end
+end
