c58cee8ac4f74bf5d9e8bdaada5cf8340bdb4ed1
Artemy Gibson <artemygibson@gmail.com>
2023-07-25 19:36:17 -0700

    Edit top_customers AR join

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,10 +2,10 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   
-  # def self.top_customers
-  #   binding.pry
-  #   Customer.joins(invoices: :transactions).where(transactions: {result: "success"})
-  # end
+  def self.top_customers
+    binding.pry
+    Customer.joins(:transactions).where(transactions: {result: "success"})
+  end
 
   def total_transactions
     transactions.count
