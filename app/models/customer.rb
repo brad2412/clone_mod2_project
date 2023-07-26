ef61d059ffc6cd3be1fb4c3a7da92452f249c7ba
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-25 16:36:45 -0800

    model basics

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,14 @@
 class Customer < ApplicationRecord
+  has_many :invoices
+  has_many :transactions, through: :invoices
   
+  # def self.top_customers
+  #   binding.pry
+  #   Customer.joins(invoices: :transactions).where(transactions: {result: "success"})
+  # end
+
+  def total_transactions
+    transactions.count
+  end
+
 end
