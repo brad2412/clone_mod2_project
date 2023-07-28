096b208c1348ba97b15fc4eff6389bf1a7a16ef8
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-28 01:45:09 -0800

    factory fixes

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.sentence }
-    unit_price { Faker::Number.number(digits: 5) }
+    unit_price { Faker::Number.between(from: 1000, to: 10000) }
     association :merchant
   end
 end
