796a714ad38b215e66c01c6a63b86fcf23c6e49e
Bradley Milton <bmilton2412@gmail.com>
2023-07-29 12:04:20 -0700

    adds user story 6, adds index spec test and index view

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -11,7 +11,17 @@ RSpec.describe "items index page", type: :feature do
     @item4 = Item.create!(name: "Chowda", description: "say it right.", unit_price: 5537, merchant_id: @merchant2.id)
   end
 
-  
+  describe "merchant items index page" do
+    it "should list the names of the merchant items" do
+      visit merchant_items_path(@merchant1)
+
+      expect(page).to have_content("Your Items")
+      expect(page).to have_content("Name: cheese")
+      expect(page).to have_content("Name: bad cheese")
+      expect(page).to have_content("Name: Bacon")
+      expect(page).to_not have_content("Name: Chowda")
+    end
+  end
 
 end
 
