20a1ea9dc44c95c1cda0f0030279f086cedbc71d
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-30 20:32:04 -0800

    formatting and test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe InvoiceItem, type: :model do
   describe "relationships" do 
-    it {should belong_to :item }
-    it {should belong_to :invoice }
+    it { should belong_to :item }
+    it { should belong_to :invoice }
+    it { should have_many(:transactions).through(:invoice) }
   end
 
   describe "validations" do
