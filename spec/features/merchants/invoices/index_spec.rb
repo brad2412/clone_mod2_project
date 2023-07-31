78655656ce8b9991062acfb632be6e247efe74b9
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-31 10:53:58 -0800

    format

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe "Merchants Invoice Index Page" do
   
   # User Story 14
   it "" do
-# As a merchant,
-# When I visit my merchant's invoices index (/merchants/:merchant_id/invoices)
-# Then I see all of the invoices that include at least one of my merchant's items
-# And for each invoice I see its id
-# And each id links to the merchant invoice show page
+    # As a merchant,
+    # When I visit my merchant's invoices index (/merchants/:merchant_id/invoices)
+    # Then I see all of the invoices that include at least one of my merchant's items
+    # And for each invoice I see its id
+    # And each id links to the merchant invoice show page
   end
 
-  #User Story 15
 end
