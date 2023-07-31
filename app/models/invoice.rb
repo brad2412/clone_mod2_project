eec3257ee08d06f6381ff238d18724eccfcfc1c7
Artemy Gibson <artemygibson@gmail.com>
2023-07-31 14:43:30 -0700

    Add items_for(merchant) method to filter items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,9 +2,8 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   # has_one :merchant, through: :items
   has_many :invoice_items
-  # has_many :items, through: :invoice_items
   has_many :transactions
-  # has_many :items, through: :invoice_items
+  has_many :items, through: :invoice_items
 
   validates :status, presence: true
 
@@ -22,12 +21,7 @@ class Invoice < ApplicationRecord
     invoice_items.sum('quantity*unit_price')
   end
 
-  # def formatted_revenue   # this method is repeated in item, merchant, and invoice_item possible rename and move to application_record?
-  #   revenue = total_revenue/100.00
-  #   formatted_amount = sprintf("$%.2f", revenue)
-  #   if formatted_amount.length > 7
-  #     formatted_amount = formatted_amount.gsub!(/(\d)(?=(\d{3})+(?!\d))/, "\\1,")
-  #   end
-  #   formatted_amount
-  # end
+  def items_for(merchant)
+    items.where('merchant_id = ?', merchant.id)
+  end
 end
