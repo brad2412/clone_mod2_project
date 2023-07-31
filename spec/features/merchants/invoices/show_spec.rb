ca4b9d13540e2932c297da74d0d96f2cee82dcac
Artemy Gibson <artemygibson@gmail.com>
2023-07-31 14:50:35 -0700

    Test that view filters invoice items by the merchant

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -2,41 +2,82 @@ require "rails_helper"
 
 RSpec.describe "Merchants Invoice Show Page" do
   before(:each) do
+    @merchant1 = Merchant.create!(name: "Dangerway", enabled: true)
+    @merchant2 = Merchant.create!(name: "FloorMart", enabled: true)
 
+    @item1 = Item.create!(name: "cheese", description: "its cheese.", unit_price: 1337, merchant_id: @merchant1.id)
+    @item2 = Item.create!(name: "bad cheese", description: "its cheese.", unit_price: 1337, merchant_id: @merchant1.id)
+    @item3 = Item.create!(name: "Bacon", description: "its bacon.", unit_price: 2337, merchant_id: @merchant1.id)
+    @item4 = Item.create!(name: "Chowda", description: "say it right.", unit_price: 5537, merchant_id: @merchant1.id)
+    @item5 = Item.create!(name: "Not Bacon", description: "idk man", unit_price: 4635, merchant: @merchant2)
+
+    @customer1 = Customer.create!(first_name: "Bob", last_name: "Smith")
+
+    @invoice1 = Invoice.create!(status: "completed", customer_id: @customer1.id, created_at: Time.new(2011,4,5))
+    
+    @invoice_item1 = InvoiceItem.create!(invoice: @invoice1, item: @item1, quantity: 5, unit_price: 3476, status: "pending")
+    @invoice_item2 = InvoiceItem.create!(invoice: @invoice1, item: @item2, quantity: 4, unit_price: 802, status: "packaged")
+    @invoice_item3 = InvoiceItem.create!(invoice: @invoice1, item: @item3, quantity: 465, unit_price: 754, status: "shipped")
+    @invoice_item4 = InvoiceItem.create!(invoice: @invoice1, item: @item4, quantity: 23, unit_price: 1509, status: "shipped")
+    @invoice_item5 = InvoiceItem.create!(invoice: @invoice1, item: @item5, quantity: 12, unit_price: 21342, status: "pending")
   end
 
   # User Story 15
-  it "" do
-    # As a merchant
-    # When I visit my merchant's invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
-    # Then I see information related to that invoice including:
-    #   - Invoice id
-    #   - Invoice status
-    #   - Invoice created_at date in the format "Monday, July 18, 2019"
-    #   - Customer first and last name
+  it "has all details of that specific invoice" do
+    visit merchant_invoice_path(@merchant1, @invoice1)
+
+    expect(page).to have_content("Invoice ##{@invoice1.id}")
+    expect(page).to have_content("Status: completed")
+    expect(page).to have_content("Created on: #{@invoice1.formatted_date}")
+    expect(page).to have_content("Customer: #{@invoice1.customer.full_name}")
   end
 
   # User Story 16
-  it "" do
-    # As a merchant
-    # When I visit my merchant invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
-    # Then I see all of my items on the invoice including:
-    #   - Item name
-    #   - The quantity of the item ordered
-    #   - The price the Item sold for
-    #   - The Invoice Item status
-    # And I do not see any information related to Items for other merchants
+  it "shows all items on the invoice and their details" do
+    visit merchant_invoice_path(@merchant1, @invoice1)
+
+    expect(page).to have_content("Items on this order:")
+
+    within("#item-#{@item1.id}") do
+      expect(page).to have_content("Item: cheese")
+      expect(page).to have_content("Quantity Ordered: 5")
+      expect(page).to have_content("Sold at: $34.76 per unit")
+      expect(page).to have_content("Item Status: pending")
+    end
+
+    within("#item-#{@item2.id}") do
+      expect(page).to have_content("Item: bad cheese")
+      expect(page).to have_content("Quantity Ordered: 4")
+      expect(page).to have_content("Sold at: $8.02 per unit")
+      expect(page).to have_content("Item Status: packaged")
+    end
+
+    within("#item-#{@item3.id}") do
+      expect(page).to have_content("Item: Bacon")
+      expect(page).to have_content("Quantity Ordered: 465")
+      expect(page).to have_content("Sold at: $7.54 per unit")
+      expect(page).to have_content("Item Status: shipped")
+    end
+
+    within("#item-#{@item4.id}") do
+      expect(page).to have_content("Item: Chowda")
+      expect(page).to have_content("Quantity Ordered: 23")
+      expect(page).to have_content("Sold at: $15.09 per unit")
+      expect(page).to have_content("Item Status: shipped")
+    end
+
+    expect(page).to_not have_content("Not Bacon")
   end
 
   # User Story 17
-  it "" do
+  xit "" do
     # As a merchant
     # When I visit my merchant invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
     # Then I see the total revenue that will be generated from all of my items on the invoice
   end
 
   # User Story 18
-  it "" do
+  xit "" do
     # As a merchant
     # When I visit my merchant invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
     # I see that each invoice item status is a select field
