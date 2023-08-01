50547bf0cae99554ee1ba82d0ecc1a76fbf20edb
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-01 15:19:04 -0800

    photo facade

---
+++ app/facades/photo_facade.rb
@@ -0,0 +1,24 @@
+class PhotoFacade
+  # def initialize(something)
+    # @thing = thing
+  # end
+
+  # all methods here? To parse?
+end
+
+
+# class SearchFacade
+#   def initialize(state)
+#     @state = state
+#   end
+
+#   def members
+#     service = CongressService.new
+
+#     json = service.members_by_state(@state)
+    
+#     @members = json[:results].map do |member_data|
+#       Member.new(member_data)
+#     end
+#   end
+# end
